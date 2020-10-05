3d685715cf4b31db2e9e13560d5f9118e12dd568
tkaaad97 <tkaaad97@gmail.com>
2020-10-05 20:13:06 +0900

    generate comparison code

--- main.go
+++ main.go
@@ -512,6 +512,30 @@ func gen(node *Node) {
     case NodeDiv:
         fmt.Printf("  cqo\n")
         fmt.Printf("  idiv rdi\n")
+    case NodeEq:
+        fmt.Printf("cmp rax, rdi\n")
+        fmt.Printf("sete al\n")
+        fmt.Printf("movzb rax, al\n")
+    case NodeNeq:
+        fmt.Printf("cmp rax, rdi\n")
+        fmt.Printf("setne al\n")
+        fmt.Printf("movzb rax, al\n")
+    case NodeLt:
+        fmt.Printf("cmp rax, rdi\n")
+        fmt.Printf("setl al\n")
+        fmt.Printf("movzb rax, al\n")
+    case NodeLe:
+        fmt.Printf("cmp rax, rdi\n")
+        fmt.Printf("setle al\n")
+        fmt.Printf("movzb rax, al\n")
+    case NodeGt:
+        fmt.Printf("cmp rdi, rax\n")
+        fmt.Printf("setl al\n")
+        fmt.Printf("movzb rax, al\n")
+    case NodeGe:
+        fmt.Printf("cmp rdi, rax\n")
+        fmt.Printf("setle al\n")
+        fmt.Printf("movzb rax, al\n")
     }
 
     fmt.Printf("  push rax\n")
