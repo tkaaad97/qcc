c39a8e20631e8da628a8f725df6a60f69213604d
tkaaad97 <tkaaad97@gmail.com>
2020-10-25 13:53:56 +0900

    fix test (still failing at pointer variable declaration)

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ assertExpr() {
   expected="$1"
   input="$2"
 
-  ./qcc "main(){${input}}" > tmp/test.s
+  ./qcc "int main(){${input}}" > tmp/test.s
   cc -o tmp/test tmp/test.s tmp/external.o
   ./tmp/test
   actual="$?"
@@ -64,24 +64,24 @@ assertExpr 0 '1>1+1+1+1;'
 assertExpr 1 '1+1+1+1>=1;'
 assertExpr 0 '1>=1+1+1+1;'
 assertExpr 1 '1+1+1+1>=1+1+1+1;'
-assertExpr 2 'a=1;b=1;c=a+b;c;'
-assertExpr 5 'a=1;b=2+3;c=-1;a+b;d=a+b+c;'
-assertExpr 2 'foo=1;bar=1;cdr=foo+bar;cdr;'
-assertExpr 123 '1+1;a=1;return 123;b=3;c=a+b;c;'
-assertExpr 5 'a1=1;a2=2;a3=a1+a2;a3*2-1;'
+assertExpr 2 'int a; int b; int c; a=1;b=1;c=a+b;c;'
+assertExpr 5 'int a; int b; int c; int d; a=1;b=2+3;c=-1;a+b;d=a+b+c;'
+assertExpr 2 'int foo;int bar;int cdr; foo=1;bar=1;cdr=foo+bar;cdr;'
+assertExpr 123 'int a; int b; int c; 1+1;a=1;return 123;b=3;c=a+b;c;'
+assertExpr 5 'int a1; int a2; int a3; a1=1;a2=2;a3=a1+a2;a3*2-1;'
 assertExpr 0 'if(0)2;'
 assertExpr 2 'if(1)2;'
-assertExpr 4 'a = 1; b = 2; if(a < b) 4; else 5;'
-assertExpr 5 'a = 10; b = 2; if(a < b) 4; else 5;'
-assertExpr 3 'a = 1; if(1) a = 2; b = 9; if(0 == 0) b = 1; a + b;'
-assertExpr 10 'b = 0; while(b<6) b = b + 5; return b;'
-assertExpr 20 'b = 0; for(a = 0; a < 10; a = a + 1) b = b + 2; return b;'
-assertExpr 2 'a = 0; { a = a + 1; a = a + 1; return a; }'
+assertExpr 4 'int a; int b; a = 1; b = 2; if(a < b) 4; else 5;'
+assertExpr 5 'int a; int b; a = 10; b = 2; if(a < b) 4; else 5;'
+assertExpr 3 'int a; int b; a = 1; if(1) a = 2; b = 9; if(0 == 0) b = 1; a + b;'
+assertExpr 10 'int b; b = 0; while(b<6) b = b + 5; return b;'
+assertExpr 20 'int a; int b; b = 0; for(a = 0; a < 10; a = a + 1) b = b + 2; return b;'
+assertExpr 2 'int a; a = 0; { a = a + 1; a = a + 1; return a; }'
 assertExpr 42 'if(1){} return 42;'
-assertExpr 50 'a = b = 0; while(a < 10){ a = a + 1; b = b + 5; } return b;'
-assertStdout "OK" 'main(){foo();}'
+assertExpr 50 'int a; int b; a = b = 0; while(a < 10){ a = a + 1; b = b + 5; } return b;'
+assertStdout "OK" 'int main(){foo();}'
 assertExpr 2 'return add(1,1);'
-assertExpr 7 'a = 1; b = 2; add(a, b) + 4;'
-assertExpr 4 'a = 0; b = &a; *b = 3; a + 1;'
+assertExpr 7 'int a; int b; a = 1; b = 2; add(a, b) + 4;'
+assertExpr 4 'int a; int* b; a = 0; b = &a; *b = 3; a + 1;'
 
 echo OK
